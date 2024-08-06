3e219884fd3f77852d6dab4800080b8e4bc542fb
iamHius <kimthu86zz@gmail.com>
2024-08-06 12:30:27 +0700

    Add project files.

---
+++ HBLC_Bookings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HBLC_Bookings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
